89a3093a7b5689504b4049d1e9a4271feb741fac
Baggnan Seydou <baggnan678@gmail.com>
2026-03-25 22:40:42 +0000

    rajout du count

--- py.py
+++ py.py
@@ -1,21 +1,20 @@
 import random
 taille = 100
 tableau = []
-n = 0
 d = int(input("De quel chiffre voulez vous connaitre l'indice : "))
 
 for i in range(taille):
     a = random.randint(0,1000)
     tableau.append(a)
     if a==d :
-      n = n+1
       p=i
       print(f"l'indice du premier {d} est de {p}")
    
 tableau2 = [z for z in tableau if z < 10]
 # .sort sert à ordonné le tableau et tt ce qui est l'intérieur des crochet du tableau2 est une condition d'ajout au tableau , z est une variable utiliser pour fixer cette condition
 tableau2.sort()
-
+# . count permet de savoir combien de fois un nombre apparait dans une liste et s'utilise exactement comme la fonction .index
+n = tableau.count(d)
 
 b = min(tableau)
 c= max(tableau)

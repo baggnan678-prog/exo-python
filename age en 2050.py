0823a467b2a4c45b53aa82e6019d155137265f03
Baggnan Seydou <baggnan678@gmail.com>
2026-03-25 23:16:15 +0000

    min

--- age en 2050.py
+++ age en 2050.py
@@ -2,4 +2,4 @@ age=int(input("Quel âge a la personne :"))
 annee=int(input("en quel annéé la personne avait cette âge : "))
 dif=2050-annee
 age=age+dif
-print("La personne aura",age,"en 2050")
+print(f"La personne aura {age} en 2050")

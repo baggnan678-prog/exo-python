7ec3cfddc7e7a11ceb0c238d2f94c7f938f73362
Baggnan Seydou <baggnan678@gmail.com>
2026-03-26 08:41:37 +0000

    changement 1

--- age en 2050.py
+++ age en 2050.py
@@ -1,5 +1,12 @@
+#code pour connaitre l'age d'une personne en 2050
+
+#déclaration des variables
 age=int(input("Quel âge a la personne :"))
 annee=int(input("en quel annéé la personne avait cette âge : "))
+
+#calcule
 dif=2050-annee
 age=age+dif
+
+#résultat finale
 print(f"La personne aura {age} en 2050")
